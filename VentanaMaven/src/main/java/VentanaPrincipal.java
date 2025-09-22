8a833adb72dbe875d33a5048a6fab51afd1c8d5a
xSurfuel2 <xSurfuel2@gmail.com>
2025-09-22 14:17:29 +0200

    v1

---
+++ VentanaMaven/src/main/java/VentanaPrincipal.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class VentanaPrincipal {
+    private JPanel JPanel1;
+    private JLabel Image1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JRadioButton acceptAMTermsRadioButton;
+    private JButton signInButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("VentanaPrincipal");
+        frame.setContentPane(new VentanaPrincipal().JPanel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,400);
+        frame.setVisible(true);
+    }
+
+
+}
